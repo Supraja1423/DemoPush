cb23de0800aa984f1901ac4fb8de51eac81e2289
Supraja <asthanadittasupraja@gmail.com>
2023-10-30 11:58:04 +0530

    Second Commit

--- DemoPush/src/pack1/Welcome.java
+++ DemoPush/src/pack1/Welcome.java
@@ -4,7 +4,8 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Welxome to GPREC");
+        System.out.println("Welcome to GPREC");
+        System.out.println("Welcome to CSM");
 	}
 
 }
